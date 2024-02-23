bc5d79e2ef361f442085a881d863f94b8ffc0a5e
svalgyx <154222657+svalgyx@users.noreply.github.com>
2024-02-23 16:39:22 +0000

    Update JaggedArray.cs

--- JaggedArray.cs
+++ JaggedArray.cs
@@ -1,42 +1,51 @@
 namespace HW3_2
 {
-    public sealed class JaggedArray : Array
+    public sealed class JaggedArray : ArrayBase
     {
-        public int Rows { get; set; }
+        private int Rows { get; set; }
         private int[][] array;
 
-        public JaggedArray(int rows, string init) : base(rows, init)
+        public JaggedArray(string init, int rows) : base(init)
         {
             Rows = rows;
             array = new int[rows][];
         }
+
+        protected override void CreateByUser() {
+            for (int i = 0; i < Rows; i++)
+            {
+                string row = Console.ReadLine();
+                string[] array_row = row.Split(' ');
+                array[i] = new int[array_row.Length];
+                for (int j = 0; j < array_row.Length; j++)
+                {
+                    array[i][j] = int.Parse(array_row[j]);
+                }
+            }
+        }
+
+        protected override void CreateByRandom() {
+            Random rnd = new Random();
+            for (int i = 0; i < Rows; i++)
+            {
+                int array_row_len = rnd.Next(1, 10);
+                array[i] = new int[array_row_len];
+                for (int j = 0; j < array_row_len; j++)
+                {
+                    array[i][j] = rnd.Next(1, 100);
+                }
+            }
+        }
+
         public void Create()
         {
             if (base.Init)
             {
-                for (int i = 0; i < Rows; i++)
-                {
-                    string row = Console.ReadLine();
-                    string[] array_row = row.Split(' ');
-                    array[i] = new int[array_row.Length];
-                    for (int j = 0; j < array_row.Length; j++)
-                    {
-                        array[i][j] = int.Parse(array_row[j]);
-                    }
-                }
+                CreateByUser();
             }
             else
             {
-                Random rnd = new Random();
-                for (int i = 0; i < Rows; i++)
-                {
-                    int array_row_len = rnd.Next(1, 10);
-                    array[i] = new int[array_row_len];
-                    for (int j = 0; j < array_row_len; j++)
-                    {
-                        array[i][j] = rnd.Next(1, 100);
-                    }
-                }
+                CreateByRandom();
             }
         }
 
