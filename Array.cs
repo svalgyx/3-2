617bfd22cd916a23b362de20f755de4b7dd434e7
svalgyx <154222657+svalgyx@users.noreply.github.com>
2024-02-23 16:38:27 +0000

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -1,23 +1,24 @@
 namespace HW3_2
 {
-    public abstract class Array
+    public abstract class ArrayBase
     {
-        public int Len { get; set; }
-
-        public bool Init { get; set; }
+        protected bool Init { get; set; }
 
         public abstract void Write();
 
         public abstract double Average();
 
-        public Array(int len, string init)
+        protected abstract void CreateByRandom();
+
+        protected abstract void CreateByUser();
+
+        public ArrayBase(string init)
         {
             Init = true;
             if (init == "no")
             {
                 Init = false;
             }
-            Len = len;
         }
     }
 }
